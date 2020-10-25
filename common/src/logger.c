c55ff14eba883772ab2be43f538ba57f62eb0ecf
rama2092 <72566974+rama2092@users.noreply.github.com>
2020-10-26 00:21:09 +0530

    Logger management (#12)
    * Enable logger common module
    
    * Test Logging code added and enabled all logs by default
    
    * Changed logging mechanism
    
    Co-authored-by: jar3m <jar3m4@gmail.com>

--- common/src/logger.c
+++ common/src/logger.c
@@ -1,6 +1,69 @@
 #include "common.h"
+#include "logger.h"
+
+//Array to maintain current log levels to be used
+static bool log_level[MAX_LOG_LEVELS] = {false};
+static bool module_logs[MAX_MODULES] = {false};
 
 void logger_init()
 {
+	int i;
+	
+	for (i = 0; i < MAX_LOG_LEVELS; i++) {
+		log_level[i] = true;
+	}
+
 	printf("Logger init\n");
+
+}
+
+int app_log(char *module, int level, char *prefix, const char *format, ...)
+{
+	int len = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	if (level < MAX_LOG_LEVELS && log_level[level] == true) {
+		char time_buff[64];
+		int millisec;
+		struct timeval tv;
+		time_t now = time(0);
+
+		gettimeofday(&tv, NULL);
+		millisec = (tv.tv_usec/1000);
+		strftime(time_buff, 64, "%H:%M:%S", localtime(&now));
+		printf("%s.%03d ", time_buff, millisec);
+		switch (level) {
+			case __LOG_ERROR__:
+				printf("[ERR");
+				break;
+			case __LOG_WARN__:
+				printf("[WRN");
+				break;
+			case __LOG_INFO__:
+				printf("[INF");
+				break;
+			case __LOG_DEBUG__:
+				printf("[DBG");
+				break;
+			case __LOG_TRACE__:
+				printf("[TRC");
+				break;
+			default:
+				printf("[UDF");
+				break;
+		}
+
+		printf(":%s] ",module);
+
+		if (prefix != NULL) {
+			printf("{%s} ", prefix);
+		}
+		len = vprintf(format, args);
+	}
+
+	va_end(args);
+
+	return len;
 }
--- test/src/test.c
+++ test/src/test.c
@@ -2,9 +2,15 @@
 
 int main(int argc, char *argv[])
 {
-	printf("Hello World\n");
-
 	logger_init();
+
+	LOG_ERROR("COMMON", "Hello World\n");
+	LOG_WARN("COMMON", "Hello World\n");		
+	LOG_INFO("COMMON", "Hello World\n");		
+	LOG_DEBUG("COMMON", "Hello World\n");		
+	LOG_TRACE_IN("COMMON", "Hello World\n");
+	LOG_TRACE_OUT("COMMON", "Hello World\n");
+
 	fault_manager_init(NULL);
 
 	return 0;
