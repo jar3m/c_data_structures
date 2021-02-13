31f1c578b3a49cd4732ce1be378f054f1ee142fa
jar3m <jar3m4@gmail.com>
2021-02-13 20:53:32 +0530

    Added struct def to linklist

--- ds/link_list/link_list.h
+++ ds/link_list/link_list.h
@@ -1,2 +1,40 @@
+#pragma once
 
-void create_link_list ();
+typedef void* t_data;
+
+
+typedef enum {
+	eSINGLE_LINKLIST,
+	eDOUBLE_LINKLIST,
+	eCIRCULAR_LINKLIST,
+}e_lltype;
+
+typedef struct elem {
+	int data;
+	struct llelem *nxt;
+	struct llelem *prv;
+} t_elem;
+
+
+typedef struct linklist {
+	e_lltype type;
+
+	int count;
+	t_llelem *head;
+	t_llelem *tail;
+} t_linklist;
+
+
+//API
+#if 0
+t_data create_link_list(e_lltype type); //me
+destroy_link_list(t_data); //me
+add_link_list(t_data, int);	//rang
+del_link_list(t_data, int);//rang	
+get_elem(t_data, int);	//may
+index(data)	//may
+count	//rang
+swap	//rang
+reverse//may
+print //me
+#endif
