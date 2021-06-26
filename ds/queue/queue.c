10d012481266ed983dbbf0c7fb9098f3309802f8
jar3m <jar3m4@gmail.com>
2021-06-26 22:31:05 +0530

    Cosmetic changes for Queues to enable Doxygen

--- ds/queue/queue.c
+++ ds/queue/queue.c
@@ -1,3 +1,8 @@
+/*! @file queue.c
+    @brief 
+    Contains definitions of routines supported by queue 
+*/
+
 #include "queue.h"
 
 bool queue_full(t_gen d);
@@ -12,36 +17,44 @@ void queue_enqueue_arr(t_gen s, t_gen data);
 t_gen queue_dequeue_arr(t_gen s);
 t_gen queue_peek_arr(t_gen d);
 
+/// Look Up function ptrs to enq elems to queue
 f_ins q_enq[] = {queue_enqueue_ll, queue_enqueue_arr};
+
+/// Look Up function ptrs to deq elems to queue
 f_deq q_deq[] = {queue_dequeue_ll, queue_dequeue_arr};
+
+/// Look Up function ptrs to peek elems to queue
 f_gen q_peek[] = {queue_peek_ll, queue_peek_arr};
 
-/*! \brief Brief description.
+/*! @brief  
  *  Destroy queue instance 
+ *  @param name     - Name of queue instance
+ *  @param max_size - Max size of queue instance
+ *  @param type     - Type of queue to be created
+ *  @param prm      - Data type specific parameters
+ *  @return         - Pointer to instance of queue
 */
 t_gen create_queue (char *name, int max_size, e_queuetype qtype, e_data_types dtype)
 {
 	t_queue *q = get_mem(1, sizeof(t_queue));
 	
 	// Init queue variables
-	q->name = name;
-	q->max_size = max_size;
-	q->type = qtype;
-	q->count = 0;
+	q->name 	= name;
+	q->max_size	= max_size;
+	q->type 	= qtype;
+	q->count	= 0;
+	q->front 	= q->rear = -1;
 	
 	// Bind queue routines
-	q->full = queue_full;
-	q->empty = queue_empty;
-	q->len = queue_size;
-	q->print = print_queue;
-	q->free = FREE_MEM;
-	
-	
-	q->enq = q_enq[qtype];
-	q->deq = q_deq[qtype];
-	q->peek = q_peek[qtype];
-	q->front = q->rear = -1;
-	// create queue space
+	q->full	 	= queue_full;
+	q->empty	= queue_empty;
+	q->len 		= queue_size;
+	q->print 	= print_queue;
+	q->enq 		= q_enq[qtype];
+	q->deq 		= q_deq[qtype];
+	q->peek 	= q_peek[qtype];
+
+	// create queue space depending on link list or array based
 	switch (qtype) 
 	{
 		case eARRAY_QUEUE_CIRC:
@@ -52,7 +65,10 @@ t_gen create_queue (char *name, int max_size, e_queuetype qtype, e_data_types dt
 					eDOUBLE_LINKLIST, dtype);
 		break;
 	}
-
+#if 0
+	s->print_data	= prm->print_data;
+	s->free 	= prm->free;
+#else
 	switch(dtype)
 	{
 		case eINT8:
@@ -72,16 +88,19 @@ t_gen create_queue (char *name, int max_size, e_queuetype qtype, e_data_types dt
 			q->print_data = print_str;
 			break;
 	}
-
+	q->free = FREE_MEM;
+#endif
 	return (t_gen)q;
 }
 
-/*! \brief Brief description.
+/*! @brief  
  *  Destroy queue instance 
+ *  @param d    - Pointer to instance of queue 
+ *  @return 	- NA
 */
-void destroy_queue (t_gen s)
+void destroy_queue (t_gen d)
 {
-	t_queue *q = (t_queue*)s;
+	t_queue *q = (t_queue*)d;
 	int i;
 
 	// Free created queue space
@@ -102,12 +121,15 @@ void destroy_queue (t_gen s)
 	free_mem(q);
 }
 
-/*! \brief Brief description.
+/*! @brief  
  *  add element in queue
+ *  @param d    - Pointer to instance of queue 
+ *  @param data - Pointer to the data to be pushed
+ *  @return 	- NA
 */
-void queue_enqueue_arr(t_gen s, t_gen data)
+void queue_enqueue_arr(t_gen d, t_gen data)
 {
-	t_queue *q = (t_queue*)s;
+	t_queue *q = (t_queue*)d;
 
 	// return if queue full
 	if (q->full(q) == true) {
@@ -124,12 +146,14 @@ void queue_enqueue_arr(t_gen s, t_gen data)
 	
 }
 
-/*! \brief Brief description.
+/*! @brief  
  *  pop front element in queue
+ *  @param d    - Pointer to instance of queue 
+ *  @return 	- Pointer to the data to be poped from
 */
-t_gen queue_dequeue_arr(t_gen s)
+t_gen queue_dequeue_arr(t_gen d)
 {
-	t_queue *q = (t_queue*)s;
+	t_queue *q = (t_queue*)d;
 	t_gen data;
 
 	// return if queue empty 
@@ -153,12 +177,14 @@ t_gen queue_dequeue_arr(t_gen s)
 
 }
 
-/*! \brief Brief description.
+/*! @brief  
  *  peek front element in queue
+ *  @param d    - Pointer to instance of queue 
+ *  @return 	- Pointer to the data to data to be peeked
 */
-t_gen queue_peek_arr(t_gen s)
+t_gen queue_peek_arr(t_gen d)
 {
-	t_queue *q = (t_queue*)s;
+	t_queue *q = (t_queue*)d;
 	t_gen data;
 
 	// return if queue empty 
@@ -173,61 +199,91 @@ t_gen queue_peek_arr(t_gen s)
 	return data;
 }
 
-/*! \brief Brief description.
+/*! @brief  
  *  add element in queue
+ *  @param d    - Pointer to instance of queue 
+ *  @param data - Pointer to the data to be pushed
+ *  @return 	- NA
 */
-void queue_enqueue_ll(t_gen s, t_gen data)
+void queue_enqueue_ll(t_gen d, t_gen data)
 {
 }
 
-/*! \brief Brief description.
+/*! @brief  
  *  pop front element in queue
+ *  @param d    - Pointer to instance of queue 
+ *  @return 	- Pointer to the data to be popped from
 */
-t_gen queue_dequeue_ll(t_gen s)
+t_gen queue_dequeue_ll(t_gen d)
 {
 }
 
-/*! \brief Brief description.
+/*! @brief  
  *  peek front element in queue
+ *  @param d    - Pointer to instance of queue 
+ *  @return 	- Pointer to the data to data to be peeked
 */
-t_gen queue_peek_ll(t_gen s)
+t_gen queue_peek_ll(t_gen d)
 {
 }
 
-/*! \brief Brief description.
+/*! @brief  
  *  Check queue full 
+ *  @param d    - Pointer to instance of queue 
+ *  @return 	- true if full
 */
 bool queue_full(t_gen d)
 {
 	return ((t_queue*)d)->count >= ((t_queue*)d)->max_size;
 }
 
-/*! \brief Brief description.
+/*! @brief  
  *  Check queue empty 
+ *  @param d    - Pointer to instance of queue 
+ *  @return 	- true if empty
 */
 bool queue_empty(t_gen d)
 {
 	return ((t_queue*)d)->count == 0;
 }
 
-/*! \brief Brief description.
+/*! @brief  
  *  Return queue size
+ *  @param d    - Pointer to instance of queue 
+ *  @return 	- size of queue
 */
 int queue_size(t_gen d)
 {
 	return ((t_queue*)d)->count;
 }
 
-/*! \brief Brief description.
+/*  @brief
+ *  Util function to get type of queue in string
+ *  @param type  - Stack Type
+ *  @return String of queue type
+*/
+static char * get_qname(e_queuetype type)
+{
+	switch(type) {
+		case eLL_QUEUE_CIRC:
+			return "LL_QUEUE";
+		case eARRAY_QUEUE_CIRC:
+			return "ARRAY_QUEUE";
+	}
+	return "UNDEFINED";
+}
+/*! @brief  
  *  print_queue_info
+ *  @param d    - Pointer to instance of queue 
+ *  @return 	- NA
 */
 void print_queue(t_gen d)
 {
 	t_queue *q = (t_queue*)d; 
 	int i;
 
-	printf("%s {max: %d} {size: %d} {front/rear: [%d:%d]} \n[",q->name,
-			q->max_size, q->count, q->front,q->rear);
+	printf("%s {max: %d} {type:%s} {size: %d} {front/rear: [%d:%d]} \n[",q->name,
+			q->max_size, get_qname(q->type), q->count, q->front,q->rear);
 	for (i = q->front; (i != -1) && (i <= q->rear); i ++) {
 	//	printf("%d ",*(int*)(q->data[i]));
 		q->print_data(q->data[i]);
--- ds/queue/queue.h
+++ ds/queue/queue.h
@@ -1,36 +1,48 @@
+/*! @file queue.h
+    @brief 
+    Contains declations of queue types, operations and structure
+*/
 #pragma once
 
 #include "common.h"
 #include "link_list.h"
 
+/// func ptr defn for queue operations
 typedef t_gen (*f_deq)(t_gen s);
 
+/// types of queue
 typedef enum {
-	eLL_QUEUE_CIRC,
-	eARRAY_QUEUE_CIRC,
+	eLL_QUEUE_CIRC,		///< Link List Based Queue
+	eARRAY_QUEUE_CIRC,	///< Array Based Queue
 } e_queuetype;
 
+/// queue struct defn
 typedef struct {
-	char *name;
-	int max_size;
-	int count;
-	int front;
-	int rear;
-	e_queuetype type;
-	t_gen *data;
+	// queue prams
+	char *name;		///< Stack instance name
+	int max_size;		///< Max Size of queue
+	int count;		///< Total elems present in queue
+	int front;		///< Queue Front Pointer
+	int rear;		///< Queue Rear Pointer
+	e_queuetype type;	///< Stack Type @see types of queue
+
+	// link List or array based queue
+	t_gen *data;		///< Ptr to link List or array based on type of queue
 	
-	f_ins enq;
-	f_deq deq;
-	f_len len;
-	f_gen peek;
-	f_full full;
-	f_empty empty;
-	f_print print;
-	f_print print_data;
-	f_free free;
+	f_ins enq;		///< routine to push elements to queue
+	f_deq deq;		///< routine to pop elements out of queue
+	f_len len;	  	///< routine to get length queue 
+	f_gen peek;	   	///< routine to peek node in queue
+	f_full full;	   	///< routine to check if queue full
+	f_empty empty;	   	///< routine to check if queue empty
+	f_print print;	   	///< routine to print queue elements
+
+	///< routines for operating on data
+	f_print print_data;	
+	f_free free;		
 } t_queue;
 
-
+// API
 t_gen create_queue (char *name, int max_size, e_queuetype type, e_data_types dtype);
 void destroy_queue (t_gen s);
 void print_queue(t_gen s);
